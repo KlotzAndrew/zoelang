3db99d9cd3e18b79fdf746aaf9321f6fad456002
Andrew Klotz <agc.klotz@gmail.com>
2022-11-06 19:27:30 +0000

    more math operators
    Signed-off-by: Andrew Klotz <agc.klotz@gmail.com>

--- vm.c
+++ vm.c
@@ -29,6 +29,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do {                \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -51,6 +57,21 @@ static InterpretResult run() {
       printf("\n");
       break;
     }
+    case OP_ADD:
+      BINARY_OP(+);
+      break;
+    case OP_SUBTRACT:
+      BINARY_OP(-);
+      break;
+    case OP_MULTIPLY:
+      BINARY_OP(*);
+      break;
+    case OP_DIVIDE:
+      BINARY_OP(/);
+      break;
+    case OP_NEGATE:
+      push(-pop());
+      break;
     case OP_RETURN: {
       printValue(pop());
       printf("\n");
@@ -61,10 +82,11 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
-  return INTERPRET_OK;
+  return run();
 }
